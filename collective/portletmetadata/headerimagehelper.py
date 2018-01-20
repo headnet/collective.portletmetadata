7ada0d1c69a6ed543e66834b8126183de6a4f2f8
Sune Broendum Woeller <sune@woeller.dk>
2018-01-20 12:51:25 +0100

    helper view for getting image url; remove PortletImageScaling and simple let the base assignment class implement IImageScaleTraversable

---
+++ collective/portletmetadata/headerimagehelper.py
@@ -0,0 +1,18 @@
+from plone.memoize.view import memoize
+from plone.namedfile.scaling import ImageScaling
+from plone.portlets.interfaces import IPortletAssignmentSettings
+from zope.publisher.interfaces import IPublishTraverse
+from zope.traversing.interfaces import ITraversable
+from zope.interface import implementer
+from Products.Five import BrowserView
+
+from .interfaces import IPortletSettings
+
+
+class PortletHeaderImageView(BrowserView):
+
+    @memoize
+    def portet_assignment_url(self, assignment):
+        assignment_mapping = assignment.__parent__.__of__(self.context)
+        assignment_url = assignment_mapping[assignment.__name__].absolute_url()
+        return assignment_url
